c07eb68e2b8fa13d62afa56dcae50340925d3537
tcwood <travis.cwood@gmail.com>
2016-10-15 16:06:30 -0700

    Implement orderBy clicks functionality

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -6,7 +6,6 @@ angular.module('shortly.links', [])
   $scope.init = function () {
     Links.getAll().then(function(links) {
       $scope.data.links = links;
-      console.log('links ', $scope.data);
     })
     .catch(function (error) {
       console.log(error);
