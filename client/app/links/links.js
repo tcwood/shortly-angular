dd487f0298660d50b8060ad4bfe6114fe5e77561
tcwood <travis.cwood@gmail.com>
2016-10-14 20:44:44 -0700

    Pass all tests. Begin rendering elements

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,4 +2,19 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+  $scope.data = {};
+  $scope.init = function () {
+    Links.getAll().then(function(links) {
+      $scope.data.links = links;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  };
+  $scope.init();
 });
+
+
+
+
+
--- client/app/services/services.js
+++ client/app/services/services.js
@@ -2,6 +2,30 @@ angular.module('shortly.services', [])
 
 .factory('Links', function ($http) {
   // Your code here
+  var getAll = function() {
+    return $http({
+      method: 'GET',
+      url: '/api/links'
+    }).then(function(res) {
+      return res.data;
+    });
+    
+  };
+
+  var addOne = function(url) {
+    return $http({
+      method: 'POST',
+      url: '/api/links',
+      data: url
+    }).then(function(res) {
+      return res;
+    });
+  };
+
+  return {
+    getAll: getAll,
+    addOne: addOne
+  };
 })
 .factory('Auth', function ($http, $location, $window) {
   // Don't touch this Auth service!!!
